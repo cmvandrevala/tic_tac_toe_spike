436b5da77e445ad03e84a6227cf563c4dde83945
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2024-02-13 19:55:58 -0600

    Add game loop

--- board.cpp
+++ board.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <cassert>
+#include <string>
 
 using namespace std;
 
@@ -91,52 +92,48 @@ public:
   }
 };
 
+class Game
+{
+private:
+  Board *board;
+  Rules *rules;
+  ConsoleBoardCreator *creator;
+
+public:
+  Game(Board *board_ptr, Rules *rules_ptr, ConsoleBoardCreator *creator_ptr)
+  {
+    board = board_ptr;
+    rules = rules_ptr;
+    creator = creator_ptr;
+  }
+
+  void start()
+  {
+    string user_input = "";
+
+    while (rules->in_progress())
+    {
+      cout << creator->formatted_board();
+      cout << "Which cell?" << endl;
+      cin >> user_input;
+      board->make_move(stoi(user_input), 'X');
+    }
+  }
+};
+
 int main()
 {
   Board board;
-
-  board.make_move(1, 'X');
-  board.make_move(3, 'X');
-  board.make_move(5, 'X');
-  board.make_move(7, 'O');
-
   Board *board_ptr = &board;
 
   ConsoleBoardCreator creator = ConsoleBoardCreator(board_ptr);
-  string formatted_board = creator.formatted_board();
-  cout << formatted_board << endl;
-
-  cout << endl;
-
-  board.make_move(1, 'W');
-
-  cout << creator.formatted_board() << endl;
+  ConsoleBoardCreator *creator_ptr = &creator;
 
   Rules rules = Rules(board_ptr);
+  Rules *rules_ptr = &rules;
 
-  bool too_low = rules.validate_input(-20);
-
-  assert(too_low == false);
-
-  bool too_high = rules.validate_input(20);
-
-  assert(too_high == false);
-
-  bool just_right_and_not_taken = rules.validate_input(6);
-
-  assert(just_right_and_not_taken == true);
-
-  bool already_taken = rules.validate_input(7);
-
-  assert(already_taken == false);
-
-  board.make_move(1, 'X');
-  board.make_move(2, 'X');
-  board.make_move(3, 'X');
-
-  bool in_progress = rules.in_progress();
-
-  assert(in_progress == false);
+  Game game = Game(board_ptr, rules_ptr, creator_ptr);
+  game.start();
 
   return 0;
 }
